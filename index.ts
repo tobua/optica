d553ce82340aa86c492b55b631299d61912b8c7b
Matthias Giger <i@matthiasgiger.com>
2024-02-21 23:24:36 +0100

    feat(scale): support for larger minimum and negative values
    release-npm

--- index.ts
+++ index.ts
@@ -10,11 +10,28 @@ export function configure(values: Partial<typeof configuration>) {
   Object.assign(configuration, values)
 }
 
-function getMinimumSize(maximumSize: number, input: number) {
-  if (input < maximumSize / 5) return maximumSize / input
-  if (input > 5) return input
-  if (input < maximumSize / 3 && input < 3) return maximumSize / input
+function getMinimumSize(maximumSize: number, input: number): number {
+  const absInput = Math.abs(input)
+  if (absInput < Math.abs(maximumSize) / 5) return maximumSize / input
+  if (absInput > 5) return input
+  if (absInput < maximumSize / 3 && absInput < 3) return maximumSize / input
   return input
+}
+
+function leftValue(minimumSize: number, maximumSize: number) {
+  if (maximumSize < 0) {
+    return round(Math.min(maximumSize, minimumSize))
+  } else {
+    return round(Math.min(maximumSize, minimumSize))
+  }
+}
+
+function rightValue(minimumSize: number, maximumSize: number) {
+  if (maximumSize < 0) {
+    return round(Math.max(maximumSize, minimumSize))
+  } else {
+    return round(Math.max(maximumSize, minimumSize))
+  }
 }
 
 export function scale(
@@ -25,5 +42,5 @@ export function scale(
   const multiplier =
     (maximumSize - minimumSize) /
     (configuration.maximumViewport - (configuration.minimumViewport - 1))
-  return `clamp(${round(minimumSize)}px, calc(${round(minimumSize - multiplier * configuration.minimumViewport)}px + ${round(multiplier * 100)}vw), ${maximumSize}px)`
+  return `clamp(${leftValue(minimumSize, maximumSize)}px, calc(${round(minimumSize - multiplier * configuration.minimumViewport)}px + ${round(multiplier * 100)}vw), ${rightValue(minimumSize, maximumSize)}px)`
 }
--- test/scale.test.ts
+++ test/scale.test.ts
@@ -1,10 +1,34 @@
 import { test, expect } from 'bun:test'
 import { scale, configure } from '../index'
 
+const getComputedValuesByViewport = (cssExpression: string) => {
+  console.log(cssExpression)
+  const regex = /calc\((-?\d*\.?\d+)px\s*\+\s*(-?\d*\.?\d+)vw\)/g
+  const matches = [...cssExpression.matchAll(regex)][0]
+
+  const pxValue = parseFloat(matches[1])
+  const vwValue = parseFloat(matches[2])
+
+  const viewportValue = (viewport: number) => Math.round(pxValue + (viewport * vwValue) / 100)
+
+  return {
+    minimum: viewportValue(320), // Should be equal to minimum breakpoint.
+    500: viewportValue(500),
+    middle: viewportValue(910), // Middle between minimum and maximum.
+    1200: viewportValue(1200),
+    maximum: viewportValue(1500), // Should be equal to maximum breakpoint.
+  }
+}
+
 test('Returns correct clamp function values.', () => {
   expect(scale(10)).toEqual('clamp(6.67px, calc(5.76px + 0.28vw), 10px)')
   expect(scale(15)).toEqual('clamp(10px, calc(8.65px + 0.42vw), 15px)')
   expect(scale(20)).toEqual('clamp(13.33px, calc(11.53px + 0.56vw), 20px)')
+
+  const regularValues = getComputedValuesByViewport(scale(10))
+  expect(regularValues.minimum).toBe(7)
+  expect(regularValues.middle).toBe(8)
+  expect(regularValues.maximum).toBe(10)
 })
 
 test('scale: second argument can either be the scalingFactor or the minimum target size.', () => {
@@ -19,6 +43,34 @@ test('scale: second argument can either be the scalingFactor or the minimum targ
   expect(scale(16, 2)).toEqual('clamp(8px, calc(5.83px + 0.68vw), 16px)')
   expect(scale(16, 4)).toEqual('clamp(4px, calc(0.75px + 1.02vw), 16px)')
   expect(scale(16, 5)).toEqual('clamp(5px, calc(2.02px + 0.93vw), 16px)')
+})
+
+test('scale: also works when the second argument is larger than the first.', () => {
+  // NOTE left and right clamp values need to switch side.
+  expect(scale(10, 20)).toEqual('clamp(10px, calc(22.71px + -0.85vw), 20px)')
+  expect(scale(5, 12)).toEqual('clamp(5px, calc(13.9px + -0.59vw), 12px)')
+  expect(scale(10, 100)).toEqual('clamp(10px, calc(124.39px + -7.62vw), 100px)')
+
+  const largerValues = getComputedValuesByViewport(scale(12, 24))
+  expect(largerValues.minimum).toBe(24)
+  expect(largerValues.middle).toBe(18)
+  expect(largerValues.maximum).toBe(12)
+})
+
+test('scale: can be used with negative input values.', () => {
+  expect(scale(-10)).toEqual('clamp(-10px, calc(-5.76px + -0.28vw), -6.67px)')
+  expect(scale(-10, -5)).toEqual('clamp(-10px, calc(-3.65px + -0.42vw), -5px)')
+  expect(scale(-10, -20)).toEqual('clamp(-20px, calc(-22.71px + 0.85vw), -10px)')
+
+  const negativeValues = getComputedValuesByViewport(scale(-10))
+  expect(negativeValues.minimum).toBe(-7)
+  expect(negativeValues.middle).toBe(-8)
+  expect(negativeValues.maximum).toBe(-10)
+
+  const largeNegativeValues = getComputedValuesByViewport(scale(-10, -100))
+  expect(largeNegativeValues.minimum).toBe(-100)
+  expect(largeNegativeValues.middle).toBe(-55)
+  expect(largeNegativeValues.maximum).toBe(-10)
 })
 
 test('configure: default parameters can be configured.', () => {
